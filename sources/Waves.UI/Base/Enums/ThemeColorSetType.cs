b64af7ede8f7c7551188f0c4d077b1546029bd4c
Egor Khindikaynen <khek@ambertape.ru>
2021-08-17 15:10:22 +0300

    Added color set interface.

---
+++ sources/Waves.UI/Base/Enums/ThemeColorSetType.cs
@@ -0,0 +1,23 @@
+namespace Waves.UI.Base.Enums
+{
+    /// <summary>
+    /// Enum of theme color set types.
+    /// </summary>
+    public enum ThemeColorSetType
+    {
+        /// <summary>
+        /// Primary color set type.
+        /// </summary>
+        Primary,
+        
+        /// <summary>
+        /// Accent color set type.
+        /// </summary>
+        Accent,
+        
+        /// <summary>
+        /// Miscellaneous color set type.
+        /// </summary>
+        Miscellaneous
+    }
+}
